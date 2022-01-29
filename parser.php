abd3614792726c0f8c7484e56acc56f0c68f5f02
Torbjörn Stabo <torbjorn@binarydesigns.se>
2022-01-29 16:32:34 +0100

    Added line number support

--- parser.php
+++ parser.php
@@ -3,6 +3,10 @@ $cfg = [];
 
 /**
  * General purpose function for accessing nodes in cfg tree
+ * Note: Returns a reference to enable manipulation. Handle accordingly.
+ * Note: Possible safety issue editing the $cfg tree directly? Getter+setter better?
+ *       I wanted to try writing it this way though, out of curiosity  :). In a real
+ *       environment I'd probably encapsulate things further.
  * 
  * @param string $path
  * @param array $cfg_root
@@ -45,20 +49,39 @@ function parseCfgDataString(string $data) {
         return $data + 0;
     }
     
+    // As is
     return $data;
 }
 
 $raw_lines = file('/tmp/config.txt');
 
 $raw_lines = array_map('trim', $raw_lines); // Trailing/leading whitespace
-$raw_lines = array_filter($raw_lines, 'strlen'); // Empty lines
-$raw_lines = array_filter($raw_lines, fn($item) => $item[0]!=='#'); // Comment lines
 
-foreach($raw_lines as $line) {
+/*
+ * Moved array_filter() lines inside foreach() to be able to use $line_number
+ */
+// $raw_lines = array_filter($raw_lines, 'strlen'); // Empty lines
+// $raw_lines = array_filter($raw_lines, fn($item) => $item[0]!=='#'); // Comment lines
+
+foreach($raw_lines as $line_number => $line) {
+    if (strlen($line) === 0) { // Empty lines
+        continue;
+    }
+    
+    if ($line[0] === '#') { // Comment lines
+        continue;
+    }
+    
     [$path, $data] = preg_split('/\s*=\s*/', $line, 2);
     
     $cfg_item = &getCfgItemByDotPath($path, $cfg);
-    $cfg_item = parseCfgDataString($data);
+    
+    try {
+        $cfg_item = parseCfgDataString($data);
+    }
+    catch (Exception $e) {
+        echo "Error parsing line ".($line_number+1).": {$e->getMessage()}\n";
+    }
 }
 unset($cfg_item);
 
